6ab4d382d1b8dfd13250b81b1fde255ea65049ea
Bob Gregory <bob.gregory@cazoo.co.uk>
2020-10-16 20:23:58 +0100

    Add billing alarm so I don't get bankrupted

--- bin/alarms.ts
+++ bin/alarms.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AlarmsStack } from '../lib/alarms-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { AlarmsStack } from "../lib/alarms-stack";
 
 const app = new cdk.App();
-new AlarmsStack(app, 'AlarmsStack');
+new AlarmsStack(app, "AlarmsStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
--- lib/alarms-stack.ts
+++ lib/alarms-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import { BillingAlert } from "@moralesl/billing-alert";
+
 
 export class AlarmsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+      const alertEmail = new cdk.CfnParameter(this, "alertEmail", {
+          type: "String",
+          description: "The email address to use for alarms"});
+
+      new BillingAlert(this, "basic-billing", {
+          amount: 10,
+          emails: [
+              alertEmail.valueAsString,
+          ]
+      })
   }
 }
